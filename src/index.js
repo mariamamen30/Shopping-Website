1b18e126cc2ed03e3c2999a11a913692f37a7b15
mariam <mariam.amen30@gmail.com>
2020-05-19 04:50:44 +0200

    adding assets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+// import Product from "./components/product";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import "./style.css";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import App from "./app";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector("#root")
+);
+
+// serviceWorker.unregister();
